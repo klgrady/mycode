6ae93dbec0033e61560cf0954055bf1d39323ccd
KL Grady <klgrady@gmail.com>
2021-08-11 21:29:06 +0000

    Rate limiter and abort

--- jinja2_challenge/server.py
+++ jinja2_challenge/server.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 from flask import Flask, render_template, request, redirect,session
+from flask import abort
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 
 app = Flask(__name__)
+
+
+limiter = Limiter(
+        app,
+        key_func=get_remote_address,
+        default_limits=["5 per day", "2 per hour"]
+        )
+
 app.config["SECRET_KEY"] = '2j34jb123kj51kj6kj1324kj1l25h32'
 
 groups = [{"hostname": "hostA","ip": "192.168.30.22", "fqdn": "hostA.localdomain"},
@@ -9,18 +21,26 @@ groups = [{"hostname": "hostA","ip": "192.168.30.22", "fqdn": "hostA.localdomain
           {"hostname": "hostC", "ip": "192.168.30.44", "fqdn": "hostC.localdomain"}]
 
 @app.route("/")
+@limiter.limit("10 per day")
 def index():
     if "allowed" in session:
         login = "allowed"
     else:
-        login = "no"
+        abort(404)
+        #login = "no"
+        
     return render_template("network.html", groups=groups, login=login)
 
 @app.route("/form")
 def form():
     return render_template("form.html")
 
+@app.route("/login")
+def trylogin():
+    return redirect("/login/mysteryperson")
+
 @app.route("/login/<name>")
+@limiter.exempt
 def login(name):
     session["allowed"] = name
     return redirect("/")
